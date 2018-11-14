84fc1ad2a65f18dad6823526b0d29d34fe1a7a78
Shea McAuley <s179049@students.aie.edu.au>
2018-11-14 12:45:19 -0800

    First commit

---
+++ Assets/Rotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Rotator : MonoBehaviour {
+
+    public float speed;
+}
+
+class RotatorSystem : ComponentSystem
+{
+    struct Components
+    {
+        public Rotator rotator;
+        public Transform transform;
+    }
+
+    protected override void OnUpdate()
+    {
+        foreach (var e in GetEntities<Components>())
+        {
+            e.transform.Rotate(0f, 200 * Time.deltaTime, 0f);
+        }
+    }
+}
